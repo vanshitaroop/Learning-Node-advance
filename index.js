06454da8200ec1f666ece8e9f4b869eecfd82081
Vanshita Roopchandani <vanshita@192.168.1.9>
2023-03-05 13:47:10 +0530

    If there are 100 routes and we want to apply middleware to only 25 routes

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from 'url';
+const route = express.Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
 import reqfilter from "./middleware.js"
 const publipath = path.join(__dirname , "public");
 const app = express();
+//change
+route.use(reqfilter);
+//change end
 app.get('/',(req,res)=>{
     res.send('Home page');
 });
-app.get('/about',reqfilter,(req,res)=>{
+//changes
+route.get('/about',(req,res)=>{
     res.send("about page");
 });
-app.get("/about",(req,res)=>{
-    res.sendFile(`${publipath}/about.html`);
-})
+route.get('/contact',(req,res)=>{
+    res.send("contact page");
+});
+//changes end
 
-
+//changes
+app.use("/",route);
+//changes end
 app.get("*",(req,res)=>{
     res.sendFile(`${publipath}/notfound.html`);
 })
+
 app.listen(3000, function(err){
     if (err) console.log("Error in server setup")
     console.log("Server listening on Port", 3000);
