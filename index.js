8e45b3554cc542aa098f9496bc079c495293e302
Vanshita Roopchandani <vanshita@192.168.1.9>
2023-03-05 12:36:12 +0530

    about ejs

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const __dirname = path.dirname(__filename)
 
 const publipath = path.join(__dirname , "public");
 const app = express();
+app.set("view engine","ejs");
+
 
 // app.get('/',(req,res)=>{
 //     res.send('Home page');
@@ -16,9 +18,21 @@ const app = express();
 app.get("/about",(req,res)=>{
     res.sendFile(`${publipath}/about.html`);
 })
+
+app.get("/profile",(_,res)=>{
+    const user={
+        name:"vanshita",
+        email:"test@gmail.com",
+        city:"ahmedabad"
+    }
+    res.render("temp",{user});
+
+});
+app.get("*",(req,res)=>{
+    res.sendFile(`${publipath}/notfound.html`);
+})
 app.listen(3000, function(err){
     if (err) console.log("Error in server setup")
     console.log("Server listening on Port", 3000);
-    console.log(__dirname)
 })
 
