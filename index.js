fa000df33847e4258c2ac623b1aa396f5c25fdf2
Vanshita Roopchandani <vanshita@192.168.1.9>
2023-03-05 12:06:14 +0530

    Filename or dir name

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
+const publipath = path.join(__dirname , "public");
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('Home page');
-});
-app.get('/about',(req,res)=>{
-    res.send("about page");
-});
+// app.get('/',(req,res)=>{
+//     res.send('Home page');
+// });
+// app.get('/about',(req,res)=>{
+//     res.send("about page");
+// });
+app.get("/about",(req,res)=>{
+    res.sendFile(`${publipath}/about.html`);
+})
 app.listen(3000, function(err){
     if (err) console.log("Error in server setup")
     console.log("Server listening on Port", 3000);
+    console.log(__dirname)
 })
 
