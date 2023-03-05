31b8b43b62f5efc5acbf762b86e272f445cfaca7
Vanshita Roopchandani <vanshita@192.168.1.9>
2023-03-05 10:37:53 +0530

    Express intro

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-import { createServer } from "http";
-// import {  red } from "chalk";
-import chalk from 'chalk';
-const {data} = require("./data.js");
-createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'Application\json'});
-    res.write(JSON.stringify(data));
-    res.end();
-}).listen("4500");
+import express from "express";
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('Home page');
+});
+app.get('/about',(req,res)=>{
+    res.send("about page");
+});
+app.listen(3000, function(err){
+    if (err) console.log("Error in server setup")
+    console.log("Server listening on Port", 3000);
+})
+
