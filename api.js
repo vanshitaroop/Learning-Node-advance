4a00a63a344151b960d2a1587713b15c3e159789
Vanshita Roopchandani <vanshita@Vanshitas-MacBook-Pro.local>
2023-03-09 12:52:12 +0530

    delete by id

--- api.js
+++ api.js
@@ -1,13 +1,14 @@
 import express from "express";
 import dbConnect from "./mongodb.js";
-
+import mongodb from "mongodb";
 // const bodyparser = require('body-parser')
 import bodyparser from "body-parser"
 const app = express();
 //body parser for parsing the data
 app.use(bodyparser.urlencoded({extended:true}))
 app.use(bodyparser.json());
-
+// alternative of body parser
+// app.use(express().jason)
 //routes
 app.get("/",async(req,res)=>{
     const data = await dbConnect();
@@ -21,4 +22,12 @@ app.post("/",async(req,res)=>{
     res.send(result);
 
 })
-app.listen(3000);
+app.delete("/:id",async(req,res)=>{
+    console.log(req.params.id);
+    let data = await dbConnect();
+    let result = await data.deleteOne({_id: new mongodb.ObjectId(req.params.id) });
+    res.send(result);
+})
+app.listen(3000,()=>{
+    console.log("Port listening on 3000");
+});
